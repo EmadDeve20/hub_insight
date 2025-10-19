0a2df67261b4fbf27cd010897ebc13780e5ae429
EmadDeve20 <emaddeve20@gmail.com>
2025-10-19 10:11:25 +0330

    create a function to create new user

--- hub_insight/users/services.py
+++ hub_insight/users/services.py
@@ -1,1 +1,26 @@
-# services
+from .models import User
+
+def create_user(first_name:str, last_name:str,
+username:str, password:str, email:str) -> User:
+    """
+    create user
+
+    Args:
+        first_name (str): first name of user
+        last_name (str): last name of user
+        username (str): username of user
+        password (str): password of user
+        email (str): email of user
+
+    Returns:
+        User: return created User
+    """
+
+    return User.objects.create_user(
+        first_name=first_name,
+        last_name=last_name,
+        password=password,
+        email=email,
+        username=username
+    )
+
