2143a7b3c3764782bc92b51549761e0f514046d4
EmadDeve20 <emaddeve20@gmail.com>
2025-10-19 10:10:43 +0330

    add first name and last name fields

--- hub_insight/users/models.py
+++ hub_insight/users/models.py
@@ -9,11 +9,11 @@ from django.contrib.auth.models import PermissionsMixin
 
 
 class BaseUserManager(BUM):
-    def create_user(self, username, is_active=True,  password=None):
+    def create_user(self, username, is_active=True,  password=None, **kwargs):
         if not username:
             raise ValueError("Users must have an username")
 
-        user = self.model(username=username, is_active=is_active)
+        user = self.model(username=username, is_active=is_active, **kwargs)
 
         if password is not None:
             user.set_password(password)
@@ -39,6 +39,9 @@ class BaseUserManager(BUM):
 
 
 class User(BaseModel, AbstractBaseUser, PermissionsMixin):
+
+    first_name = models.CharField(max_length=255, default="")
+    last_name = models.CharField(max_length=255, default="")
 
     username = models.CharField(max_length=255, unique=True)
 
