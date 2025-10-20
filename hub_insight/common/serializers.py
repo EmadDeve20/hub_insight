fe21bf421a9736e26aa3af91328ad9a8a6a98403
EmadDeve20 <emaddeve20@gmail.com>
2025-10-21 00:20:21 +0330

    create default serializes for list of data and filter for query params

---
+++ hub_insight/common/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+class PaginationFilterSerializer(serializers.Serializer):
+    p = serializers.IntegerField(
+        required=False, help_text="A page number within the paginated result set."
+    )
+    page_size = serializers.IntegerField(
+        required=False, help_text="Number of results to return per page."
+    )
+    all = serializers.BooleanField(default=False, required=False)
+
+
+
+class SwaggerListSerializer(serializers.Serializer):
+    count = serializers.IntegerField()
+    next = serializers.CharField(allow_null=True)
+    previous = serializers.CharField(allow_null=True)
+    page_size = serializers.IntegerField()
+    results = serializers.ListField()
+
