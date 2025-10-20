3753158f8cfeecc7df829810073183547e56c2cb
EmadDeve20 <emaddeve20@gmail.com>
2025-10-20 20:15:28 +0330

    update typing

--- hub_insight/utils/typing.py
+++ hub_insight/utils/typing.py
@@ -1,5 +1,17 @@
-from typing import TypedDict
+from typing import TypedDict, Optional
 
 class JobDetail(TypedDict):
     help: str
     name: str
+    version: str = "v1"
+
+
+class JobVariable(TypedDict):
+    name :str
+    variable_type: type
+    default: Optional[type]
+
+
+class JobReturnAnnotation(TypedDict):
+    return_type: type
+
