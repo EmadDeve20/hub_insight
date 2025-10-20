1969559967fe75e0c598fe468732f9275746e9f3
EmadDeve20 <emaddeve20@gmail.com>
2025-10-21 00:24:02 +0330

    add versioning for urls and add urls for jobs app

--- hub_insight/api/urls.py
+++ hub_insight/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('users/', include(('hub_insight.users.urls', 'users')))
+    path('v1/users/', include(('hub_insight.users.urls', 'users'))),
+    path('v1/jobs/', include(('hub_insight.jobs.urls', 'jobs'))),
 ]
