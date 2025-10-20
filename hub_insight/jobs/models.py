6d5afc28bb207208e2e54b0cffed738464e2b944
EmadDeve20 <emaddeve20@gmail.com>
2025-10-20 20:14:49 +0330

    update job model

--- hub_insight/jobs/models.py
+++ hub_insight/jobs/models.py
@@ -6,5 +6,11 @@ from hub_insight.common.models import BaseModel
 class Job(BaseModel):
     name = models.CharField(max_length=255, unique=True)
     help = models.CharField(max_length=255)
+    script_filename = models.CharField(max_length=255, default="test.py")
+    version = models.CharField(max_length=255, default="v1")
+
+
+    def __str__(self):
+        return self.name
 
 
