7399b733aca3b115fc8f53eb8d16c2a1fd37e84a
EmadDeve20 <emaddeve20@gmail.com>
2025-10-20 17:28:16 +0330

    create url for create user api

--- hub_insight/users/urls.py
+++ hub_insight/users/urls.py
@@ -1,1 +1,10 @@
-# urls
+from django.urls import path
+
+from .apis import CreateUserApi
+
+app_name = "users"
+
+urlpatterns = [
+    path("create_user/", CreateUserApi.as_view(), name="create_user")
+]
+
