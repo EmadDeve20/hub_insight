f04418faa3bb6a88b40c0b6e06c88fbb86096707
EmadDeve20 <emaddeve20@gmail.com>
2025-10-21 11:36:20 +0330

    improve PageNumberPagination class

--- hub_insight/api/pagination.py
+++ hub_insight/api/pagination.py
@@ -2,18 +2,17 @@ from collections import OrderedDict
 
 from rest_framework.pagination import PageNumberPagination as _PageNumberPagination
 from rest_framework.response import Response
-from rest_framework.exceptions import NotFound
-from django.core.paginator import InvalidPage
+
 
 class PageNumberPagination(_PageNumberPagination):
     # TODO: Read this from env file
     # page_size = int(REST_FRAMEWORK["PAGE_SIZE"])
     # max_page_size = int(REST_FRAMEWORK["MAX_PAGE_SIZE"])
-    
+
     page_size = 5
     max_page_size = 10
     superuser_max_page_size =100
-    
+
     page_query_param = "p"
     page_size_query_param = "page_size"
 
@@ -31,6 +30,7 @@ class PageNumberPagination(_PageNumberPagination):
             )
         )
 
+
     def paginate_queryset(self, queryset, request, view=None):
         """
         Paginate a queryset if required, either returning a
@@ -40,27 +40,8 @@ class PageNumberPagination(_PageNumberPagination):
         page_number = request.GET.get("all")
         if page_number == "true":
             return None
-        page_size = self.get_page_size(request)
-        if not page_size:
-            return None
-
-        paginator = self.django_paginator_class(queryset, page_size)
-        page_number = self.get_page_number(request, paginator)
-
-        try:
-            self.page = paginator.page(page_number)
-        except InvalidPage as exc:
-            msg = self.invalid_page_message.format(
-                page_number=page_number, message=str(exc)
-            )
-            raise NotFound(msg)
-
-        if paginator.num_pages > 1 and self.template is not None:
-            # The browsable API should display pagination controls.
-            self.display_page_controls = True
-
-        self.request = request
-        return self.page
+    
+        return super().paginate_queryset(queryset, request, view)
 
 
     def get_page_size(self, request):
