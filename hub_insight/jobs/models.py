a5b4b3edb1605443c0cb449ad25e0cbcdf3a52c2
EmadDeve20 <emaddeve20@gmail.com>
2025-10-21 14:02:59 +0330

    add model for variable and response for jobs

--- hub_insight/jobs/models.py
+++ hub_insight/jobs/models.py
@@ -14,3 +14,48 @@ class Job(BaseModel):
         return self.name
 
 
+
+class TypeChoices(models.TextChoices):
+
+        interger = ('int', 'Integer')
+        string = ('str', 'String')
+        boolean = ('bool', 'Boolean')
+        float = ('float', 'Float')
+
+        
+
+MAP_PYTHON_VAR = {
+    int: TypeChoices.interger,
+    str: TypeChoices.string,
+    bool: TypeChoices.boolean,
+    float: TypeChoices.float
+}
+
+class Variable(BaseModel):
+
+
+
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name='variables')
+    name = models.CharField(max_length=255)
+    var_type = models.CharField(max_length=255,
+                                choices=TypeChoices.choices,
+                                )
+    default = models.CharField(max_length=255, null=True, default=None)
+
+    def __str__(self):
+        if self.default:
+            return f"{self.name}:{self.var_type} = {self.default}"
+        return f"{self.name}:{self.var_type}"
+
+
+
+class Response(BaseModel):
+    response_type = models.CharField(max_length=255,
+                                choices=TypeChoices.choices,
+                                )
+
+    job = models.OneToOneField(Job, on_delete=models.CASCADE, related_name='response_type')
+
+    def __str__(self):
+        return f" -> {self.response_type}"
+
