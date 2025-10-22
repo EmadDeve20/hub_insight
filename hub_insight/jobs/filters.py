ba3c840c48eed2d323aa4ab2723b2abf5f0fc26f
EmadDeve20 <emaddeve20@gmail.com>
2025-10-22 18:53:10 +0330

    add filters for jobs

---
+++ hub_insight/jobs/filters.py
@@ -0,0 +1,18 @@
+from django_filters import FilterSet, CharFilter
+
+from hub_insight.utils.explore.searcher import searcher
+
+class JobFilterSet(FilterSet):
+    search = CharFilter(method="filter_search")
+
+
+    def filter_search(self, queryset, name, value):
+
+        fields = [
+            "name",
+            "help",
+            # "variables__name",
+        ]
+
+        return searcher(queryset, fields, value)
+
