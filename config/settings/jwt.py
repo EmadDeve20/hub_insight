8d09abb9b301f01fa4d050999469619a24915234
EmadDeve20 <emaddeve20@gmail.com>
2025-10-22 22:56:22 +0330

    update settings for jwt

--- config/settings/jwt.py
+++ config/settings/jwt.py
@@ -1,26 +1,13 @@
-import datetime
+from datetime import timedelta
 
 from config.env import env
 
-# For more settings
-# Read everything from here - https://styria-digital.github.io/django-rest-framework-jwt/#additional-settings
-
-# Default to 7 days
-JWT_EXPIRATION_DELTA_SECONDS = env("JWT_EXPIRATION_DELTA_SECONDS", default=60 * 60 * 24 * 7)
-JWT_AUTH_COOKIE = env("JWT_AUTH_COOKIE", default="jwt")
-JWT_AUTH_COOKIE_SAMESITE = env("JWT_AUTH_COOKIE_SAMESITE", default="Lax")
-JWT_AUTH_HEADER_PREFIX = env("JWT_AUTH_HEADER_PREFIX", default="Bearer")
-
 
 JWT_AUTH = {
-    "JWT_GET_USER_SECRET_KEY": "hub_insight.authentication.services.auth_user_get_jwt_secret_key",
-    "JWT_RESPONSE_PAYLOAD_HANDLER": "hub_insight.authentication.services.auth_jwt_response_payload_handler",
-    "JWT_EXPIRATION_DELTA": datetime.timedelta(seconds=JWT_EXPIRATION_DELTA_SECONDS),
-    "JWT_ALLOW_REFRESH": False,
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=env.int("TTL_ACCESS_TOKEN", 900)),
+    'REFRESH_TOKEN_LIFETIME': timedelta(seconds=env.int("TTL_REFRESH_TOKEN", 86400)),
 
-    "JWT_AUTH_COOKIE": JWT_AUTH_COOKIE,
-    "JWT_AUTH_COOKIE_SECURE": True,
-    "JWT_AUTH_COOKIE_SAMESITE": JWT_AUTH_COOKIE_SAMESITE,
-
-    "JWT_AUTH_HEADER_PREFIX": JWT_AUTH_HEADER_PREFIX
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': True,
 }
