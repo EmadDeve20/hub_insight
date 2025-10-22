614609a34203630e6c230f4cc54b548f66a4befc
EmadDeve20 <emaddeve20@gmail.com>
2025-10-22 22:34:14 +0330

    add url for authentication app

--- hub_insight/api/urls.py
+++ hub_insight/api/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('v1/users/', include(('hub_insight.users.urls', 'users'))),
     path('v1/jobs/', include(('hub_insight.jobs.urls', 'jobs'))),
+    path('v1/authentication/', include(('hub_insight.authentication.urls', 'authentication'))),
 ]
