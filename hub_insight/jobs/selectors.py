d23234b4004427ab675ef17b7097c86db0c6fafd
EmadDeve20 <emaddeve20@gmail.com>
2025-10-22 18:55:51 +0330

    fix TODO: add filter for list of jobs

--- hub_insight/jobs/selectors.py
+++ hub_insight/jobs/selectors.py
@@ -1,9 +1,22 @@
 from django.db.models import QuerySet
 
 from .models import Job
+from .filters import JobFilterSet
 
-# TODO: add filter for this selector
 def get_list_job(filter:dict={}) -> QuerySet[Job]:
-    return Job.objects.all()
+    """
+    get list of jobs
+
+    Args:
+        filter (dict, optional): dictionary filters of fields. Defaults to {}.
+
+    Returns:
+        QuerySet[Job]: return queryset of Job
+    """
+
+    qs = Job.objects.all()
+
+    return JobFilterSet(filter, qs).qs
+
 
 
