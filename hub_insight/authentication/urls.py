484e793fae048314f37916d67e9388b1a5d1687d
EmadDeve20 <emaddeve20@gmail.com>
2025-10-22 22:56:35 +0330

    add logout url

--- hub_insight/authentication/urls.py
+++ hub_insight/authentication/urls.py
@@ -3,6 +3,7 @@ from rest_framework_simplejwt.views import(
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
+    TokenBlacklistView,
  )
 
 urlpatterns = [
@@ -10,5 +11,6 @@ urlpatterns = [
             path('login/', TokenObtainPairView.as_view(),name="login"),
             path('refresh/', TokenRefreshView.as_view(),name="refresh"),
             path('verify/', TokenVerifyView.as_view(),name="verify"),
+            path('logout/', TokenBlacklistView.as_view(),name="verify"),
             ])), name="jwt"),
 ]
