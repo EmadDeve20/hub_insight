09c8eae3beb4717f0bd688a9388a45c5fbf36906
EmadDeve20 <emaddeve20@gmail.com>
2025-10-23 19:32:22 +0330

    add url for tasks app

--- hub_insight/api/urls.py
+++ hub_insight/api/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('v1/users/', include(('hub_insight.users.urls', 'users'))),
     path('v1/jobs/', include(('hub_insight.jobs.urls', 'jobs'))),
     path('v1/authentication/', include(('hub_insight.authentication.urls', 'authentication'))),
+    path('v1/tasks/', include(('hub_insight.tasks.urls', 'tasks'))),
 ]
