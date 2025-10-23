12b1ae9849dd7c660f020dea3aa9c161f8947b93
EmadDeve20 <emaddeve20@gmail.com>
2025-10-23 20:19:20 +0330

    update task model. rename and more

--- hub_insight/tasks/models.py
+++ hub_insight/tasks/models.py
@@ -2,28 +2,34 @@ from django.db import models
 
 from hub_insight.common.models import BaseModel
 
-from django_celery_beat.models import PeriodicTask
+from django_celery_beat.models import PeriodicTask, CrontabSchedule
 
 
 
-class ScheduleTask(BaseModel):
+class Task(BaseModel):
+    name = models.CharField(max_length=255)
     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
     job = models.ForeignKey("jobs.Job", on_delete=models.CASCADE)
-    enable = models.BooleanField(default=True)
-    celery_periodic_task = models.ForeignKey(PeriodicTask, on_delete=models.CASCADE)
+    enabled = models.BooleanField(default=True)
+    celery_periodic_task = models.ForeignKey(PeriodicTask,
+                                             on_delete=models.CASCADE,
+                                             related_name='custom_task_periodic',)
+    celery_cron_schedule = models.ForeignKey(CrontabSchedule,
+                                             on_delete=models.CASCADE,
+                                             related_name='custom_task_crontab')
     variables = models.JSONField()
 
 
     def save(self, *args, **kwarfg):
-        celery_periodic_task.enable = self.enable
+        self.celery_periodic_task.enabled = self.enabled
         
-        celery_periodic_task.save()
+        self.celery_periodic_task.save()
 
         return super().save(*args, **kwarfg)
 
 
     def delete(self, *args, **kwarfg):
-        celery_periodic_task.delete()
+        self.celery_periodic_task.delete()
 
         return super().delete(*args, **kwarfg)
 
