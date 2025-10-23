259e5dff2dc72566a9ee181e6abde26d43038ff9
EmadDeve20 <emaddeve20@gmail.com>
2025-10-23 19:25:59 +0330

    create task model

--- hub_insight/tasks/models.py
+++ hub_insight/tasks/models.py
@@ -1,3 +1,30 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+from hub_insight.common.models import BaseModel
+
+from django_celery_beat.models import PeriodicTask
+
+
+
+class ScheduleTask(BaseModel):
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    job = models.ForeignKey("jobs.Job", on_delete=models.CASCADE)
+    enable = models.BooleanField(default=True)
+    celery_periodic_task = models.ForeignKey(PeriodicTask, on_delete=models.CASCADE)
+    variables = models.JSONField()
+
+
+    def save(self, *args, **kwarfg):
+        celery_periodic_task.enable = self.enable
+        
+        celery_periodic_task.save()
+
+        return super().save(*args, **kwarfg)
+
+
+    def delete(self, *args, **kwarfg):
+        celery_periodic_task.delete()
+
+        return super().delete(*args, **kwarfg)
+
+
