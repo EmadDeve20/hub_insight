55548e596a6d65690e80bcaabe08e33b326c899d
EmadDeve20 <emaddeve20@gmail.com>
2025-10-23 19:25:24 +0330

    add map variables to python and serializer

---
+++ hub_insight/common/mapper.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from hub_insight.jobs.models import TypeChoices
+
+
+MAP_JOB_TYPE_TO_PYTHON_TYPE = {
+    TypeChoices.interger: int,
+    TypeChoices.string: str,
+    TypeChoices.float: float,
+    TypeChoices.boolean: bool
+}
+
+
+MAP_JOB_TYPE_TO_SERIALIZER = {
+    TypeChoices.interger: serializers.IntegerField,
+    TypeChoices.string: serializers.CharField,
+    TypeChoices.float: serializers.FloatField,
+    TypeChoices.boolean: serializers.BooleanField,
+}
