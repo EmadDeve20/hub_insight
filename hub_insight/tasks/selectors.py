e11a8fe425bf7ea521d47a832476bb654bafe967
EmadDeve20 <emaddeve20@gmail.com>
2025-10-24 15:02:21 +0330

    create selector to get list of tasks

--- hub_insight/tasks/selectors.py
+++ hub_insight/tasks/selectors.py
@@ -1,6 +1,9 @@
 from django.utils.translation import gettext_lazy as _
+from django.db.models import QuerySet
 
 from rest_framework.exceptions import NotFound
+
+from hub_insight.users.models import User
 
 from .models import Task
 
@@ -23,5 +26,31 @@ def get_task_by_name(name:str) -> Task:
         return Task.objects.get(name=name)
     except Task.DoesNotExist:
         raise NotFound(_("task does not exist!"))
+
+
+
+# TODO: add filter dict
+def get_list_of_task(filter:dict={},
+user:User|None = None) -> QuerySet[Task]:
+    """
+    get list of tasks
+
+    Args:
+        filter (dict, optional): filter fields. Defaults to {}.
+        user (User | None, optional): user for filter by user. Defaults to None.
+        if this is None, this is will be all of tasks.
+
+    Returns:
+        QuerySet[Task]: return selected queryset of task
+    """
+
+    if not user or user.is_superuser:
+        qs = Task.objects.all()
+
+    elif user or not user.is_superuser:
+        qs = Task.objects.filter(user=user)
+
+    return qs
+
 
 
