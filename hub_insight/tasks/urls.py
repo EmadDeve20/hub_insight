81f7f8259b37d36d5af0e7b39868dddae656938b
EmadDeve20 <emaddeve20@gmail.com>
2025-10-24 15:29:31 +0330

    add url for UpdateDeleteRetriveTaskApi API

--- hub_insight/tasks/urls.py
+++ hub_insight/tasks/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from .apis import CreateListScheduleTaskApi
+from .apis import (
+    CreateListScheduleTaskApi,
+    UpdateDeleteRetriveTaskApi,
+)
 
 app_name = "tasks"
 
 urlpatterns = [
-    path("", CreateListScheduleTaskApi.as_view(), name="create_list_schedule_task"),
+    path("", CreateListScheduleTaskApi.as_view(),
+         name="create_list_task"),
+    
+    path("<int:id>/", UpdateDeleteRetriveTaskApi.as_view(),
+         name="update_delete_retrive_task"),
 ]
