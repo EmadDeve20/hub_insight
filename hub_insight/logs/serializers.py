24ac7bb2dc6e1baede3e54c2600fc6fb3ffad7d6
EmadDeve20 <emaddeve20@gmail.com>
2025-10-24 19:48:10 +0330

    create serializer to get task by id

--- hub_insight/logs/serializers.py
+++ hub_insight/logs/serializers.py
@@ -14,10 +14,31 @@ class OutputLogTaskListSerializer(serializers.ModelSerializer):
             "created_at",
             "job",
             "is_ok",
-            "variables"
+            "variables",
+            "job_version",
+            "response_type",
         ]
 
 
 class OutputLogTaskListSwaggerSerializer(SwaggerListSerializer):
     results = serializers.ListField(child=OutputLogTaskListSerializer())
 
+
+
+class OutputRetrieveLogTaskSerializer(serializers.ModelSerializer):
+    job = serializers.CharField(source="task.job.name", default=None)
+
+    class Meta:
+        model = LogTask
+        fields = [
+            "id",
+            "created_at",
+            "job",
+            "is_ok",
+            "variables",
+            "error_message",
+            "job_help",
+            "response_type",
+            "response_value",
+            "job_version",
+        ]
