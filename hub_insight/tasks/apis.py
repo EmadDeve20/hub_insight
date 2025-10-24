f9c2c1471baa6fe553d5794896569c98c3efad7d
EmadDeve20 <emaddeve20@gmail.com>
2025-10-24 10:27:09 +0330

    add output response for create task post method

--- hub_insight/tasks/apis.py
+++ hub_insight/tasks/apis.py
@@ -1,12 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 
 from drf_spectacular.utils import extend_schema
 
 
 from hub_insight.api.mixins import ApiAuthMixin
-from .serializers import InputCreateTaskSerializer
+from .serializers import (
+    InputCreateTaskSerializer,
+    OutputTaskSerializer,
+)
 from .services import create_task
 
 class CreateListScheduleTaskApi(ApiAuthMixin, APIView):
@@ -14,7 +18,8 @@ class CreateListScheduleTaskApi(ApiAuthMixin, APIView):
     # TODO: add service  to create task 
     @extend_schema(
         tags=["Task"],
-        request=InputCreateTaskSerializer
+        request=InputCreateTaskSerializer,
+        responses=OutputTaskSerializer
     )
     def post(self, request):
         
@@ -25,8 +30,9 @@ class CreateListScheduleTaskApi(ApiAuthMixin, APIView):
         task = create_task(user=request.user,
                     **serializer.validated_data)
 
-        print(f"{task=}")
+        output_serializer = OutputTaskSerializer(task)
 
-        return Response()
+        return Response(output_serializer.data, status.HTTP_201_CREATED)
+
 
 
