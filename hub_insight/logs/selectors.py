c4cc82a88ec05f1c743abc642f72aaa3ecada6a5
EmadDeve20 <emaddeve20@gmail.com>
2025-10-24 19:47:59 +0330

    create a selector to get log task by id

--- hub_insight/logs/selectors.py
+++ hub_insight/logs/selectors.py
@@ -1,11 +1,14 @@
 from django.db.models import QuerySet
+from django.utils.translation import gettext_lazy as _
+
+from rest_framework.exceptions import NotFound
 
 from hub_insight.tasks.models import LogTask
 from hub_insight.users.models import User 
 
 
 # TODO: add filter here
-def get_task_log_list(user:User|None,
+def get_task_log_list(user:User|None=None,
 filter:dict={}) -> QuerySet[LogTask]:
     """
     get list of tasks log
@@ -21,10 +24,31 @@ filter:dict={}) -> QuerySet[LogTask]:
     if not user:
         qs = LogTask.objects.all()
     else:
-        qs = LogTask.objects.filter(user=user)
+        qs = LogTask.objects.filter(task__user=user)
     
 
     return qs
+
+
+def get_task_log_by_id(task_log_id:int, user:User|None=None) -> LogTask:
+    """
+    get task log by id
+
+    Args:
+        task_log_id (int): id of log
+        user (User | None, optional): user to filter by user. Defaults to None.
+
+    Raises:
+        NotFound: raise notfound if log does not exist
+
+    Returns:
+        LogTask: retrun selected Log object
+    """
+
+    try:
+        return get_task_log_list(user=user).get(id=task_log_id)
+    except  LogTask.DoesNotExist:
+        raise NotFound(_("log does not exist!"))
 
 
 
