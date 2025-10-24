dea5f000a4f319d72101c0dd75285f366ba6dec3
EmadDeve20 <emaddeve20@gmail.com>
2025-10-24 19:49:08 +0330

    add url to get log by id

--- hub_insight/logs/urls.py
+++ hub_insight/logs/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
-from .apis import ListLogTaskApi
+from .apis import ListLogTaskApi, RetrieveLogTaskApi
 
 name = "logs"
 
 urlpatterns = [
     path("task-log-list/", ListLogTaskApi.as_view(),
-        name="get_list_of_task_logs")
+        name="get_list_of_task_logs"),
+
+    path("task-log/<int:id>/", RetrieveLogTaskApi.as_view(),
+        name="retrieve_task_logs"),
 ]
 
