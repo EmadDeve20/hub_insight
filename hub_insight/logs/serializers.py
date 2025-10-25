b034ffcd03e02025c526e5fd05c771c38ff78577
EmadDeve20 <emaddeve20@gmail.com>
2025-10-25 18:36:57 +0330

    add update log serializer

--- hub_insight/logs/serializers.py
+++ hub_insight/logs/serializers.py
@@ -3,31 +3,12 @@ from rest_framework import serializers
 from hub_insight.common.serializers import SwaggerListSerializer
 from hub_insight.tasks.models import LogTask
 from hub_insight.tasks.serializers import OutputTaskSerializer
+from hub_insight.users.serializers import OutputUserSerializer
 
 class OutputLogTaskListSerializer(serializers.ModelSerializer):
 
-    task = OutputTaskSerializer()
-
-    class Meta:
-        model = LogTask
-        fields = [
-            "id",
-            "created_at",
-            "task",
-            "is_ok",
-            "variables",
-            "job_version",
-            "response_type",
-        ]
-
-
-class OutputLogTaskListSwaggerSerializer(SwaggerListSerializer):
-    results = serializers.ListField(child=OutputLogTaskListSerializer())
-
-
-
-class OutputRetrieveLogTaskSerializer(serializers.ModelSerializer):
     job = serializers.CharField(source="task.job.name", default=None)
+    user = OutputUserSerializer(source="task.user", default=None)
 
     class Meta:
         model = LogTask
@@ -37,9 +18,32 @@ class OutputRetrieveLogTaskSerializer(serializers.ModelSerializer):
             "job",
             "is_ok",
             "variables",
+            "job_version",
+            "response_type",
+            "user",
+        ]
+
+
+class OutputLogTaskListSwaggerSerializer(SwaggerListSerializer):
+    results = serializers.ListField(child=OutputLogTaskListSerializer())
+
+
+
+class OutputRetrieveLogTaskSerializer(serializers.ModelSerializer):
+    task = OutputTaskSerializer()
+
+
+    class Meta:
+        model = LogTask
+        fields = [
+            "id",
+            "created_at",
+            "is_ok",
+            "variables",
             "error_message",
             "job_help",
             "response_type",
             "response_value",
             "job_version",
+            "task",
         ]
