dce7da20b06ffbbbdfa398813ed861405fd647f7
EmadDeve20 <emaddeve20@gmail.com>
2025-10-25 18:39:50 +0330

    add filter arg serializer for task log

--- hub_insight/logs/apis.py
+++ hub_insight/logs/apis.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import serializers
 
 from drf_spectacular.utils import extend_schema
 
@@ -21,14 +22,33 @@ from .serializers import (
 
 class ListLogTaskApi(ApiAuthMixin, APIView):
 
+    class InputArgSerializer(serializers.Serializer):
+        search = serializers.CharField(required=False,
+                                       help_text="search on fields: job,")
+        
+        boolean_choices = ("true", "false")
+
+        is_enabled = serializers.ChoiceField(required=False, choices=boolean_choices)
+        job_ids = serializers.CharField(required=False, help_text="id of jobs. seprated by ,")
+        user_ids = serializers.CharField(required=False, help_text="id of users. seprated by ,")
+        order_by = serializers.CharField(required=False, help_text="order by response fields."
+                                         "for example order by jobs name: `job__name`"
+                                         " or reverse: `-job__name`. you can use more seprated by `,`")
+        is_ok = serializers.ChoiceField(required=False, choices=boolean_choices)
+
+
     @extend_schema(
         tags=["Logs"],
         responses=OutputLogTaskListSwaggerSerializer,
-        parameters=[PaginationFilterSerializer]
+        parameters=[PaginationFilterSerializer, InputArgSerializer]
     )
     def get(self, request):
         
-        queryset = get_task_log_list(user=request.user)
+        filter_serializer = self.InputArgSerializer(data=request.query_params)
+        filter_serializer.is_valid(raise_exception=True)
+
+        queryset = get_task_log_list(user=request.user,
+                                     filter=filter_serializer.validated_data)
 
         return get_paginated_response(
             serializer_class=OutputLogTaskListSerializer,
